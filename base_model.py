9fed9e2ae777e7a7ea6666e1451f6a3c78425281
Matthew Dong <matthew.dong@nyu.edu>
2023-11-30 00:15:17 -0500

    gpt2 base model

--- base_model.py
+++ base_model.py
@@ -1,9 +1,14 @@
-from transformers import GPT2LMHeadModel, GPT2Tokenizer
+import torch
+from transformers import GPT2LMHeadModel, GPT2Tokenizer #pylint: disable=import-error
 
 # Load pre-trained model and tokenizer
 model_name = "gpt2"
 model = GPT2LMHeadModel.from_pretrained(model_name)
 tokenizer = GPT2Tokenizer.from_pretrained(model_name)
+
+# Check if CUDA (GPU support) is available and move the model to GPU if possible
+if torch.cuda.is_available():
+    model = model.cuda()
 
 # Example text strings
 text_examples = [
@@ -15,5 +20,23 @@ text_examples = [
 # Tokenizing and generating responses for the text examples
 for text in text_examples:
     inputs = tokenizer.encode(text, return_tensors="pt")
-    outputs = model.generate(inputs, max_length=50, num_return_sequences=1)
+
+    # Generate attention mask (1 for real tokens and 0 for padding tokens)
+    attention_mask = inputs.ne(tokenizer.pad_token_id).int()
+
+    # Move inputs and attention mask to GPU if available
+    if torch.cuda.is_available():
+        inputs = inputs.cuda()
+        attention_mask = attention_mask.cuda()
+
+    outputs = model.generate(
+        inputs,
+        max_length=50,
+        num_return_sequences=1,
+        attention_mask=attention_mask,
+        pad_token_id=tokenizer.eos_token_id,
+        verbose=True  # Verbose output
+    )
+
     print(f"Input: {text}\nOutput: {tokenizer.decode(outputs[0], skip_special_tokens=True)}\n")
+
